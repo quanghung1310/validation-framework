7375f3b08f4226f69628fd9348054513422b8fe3
Hùng Trần <tranphihung312@gmail.com>
2020-12-20 15:19:53 +0700

    Create project

--- ValidationFramework/WebApp/Controllers/HomeController.cs
+++ ValidationFramework/WebApp/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using ValidationFramework.Models;
+using WebApp.Models;
 
-namespace ValidationFramework.Controllers
+namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
--- ValidationFramework/WebApp/Models/ErrorViewModel.cs
+++ ValidationFramework/WebApp/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ValidationFramework.Models
+namespace WebApp.Models
 {
     public class ErrorViewModel
     {
