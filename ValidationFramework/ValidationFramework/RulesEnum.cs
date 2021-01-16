7e75020b4ee8272b4f27c60024f6d4ee8c30dce2
Hùng Trần <tranphihung312@gmail.com>
2021-01-16 12:16:27 +0700

    Enum rules, defaut factory

---
+++ ValidationFramework/ValidationFramework/RulesEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValidationFramework
+{
+    public static class Rules
+    {
+        public const string
+            Mail = "email",
+            MaxLength = "maxlength",
+            MinLength = "minlength",
+            Numeric = "numeric",
+            Phone = "phone",
+            Range = "range",
+            Url = "url",
+            RegularExpression = "regularexpression",
+            Required = "required";
+        public bool Exist()
+        {
+            Enum.GetValues(typeof(Rules)).Cast<Rules>();
+            
+        }
+    }
+
+}
