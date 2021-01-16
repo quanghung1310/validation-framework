f95c18c1daf0ff4b7eda91a94c72db9df8214247
sacphan <56332124+sacphan@users.noreply.github.com>
2021-01-16 12:17:57 +0700

    update validator

---
+++ ValidationFramework/ValidationFramework/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidationFramework
+{
+    public  class Validation
+    {
+      
+       public void Validator(object Request,Dictionary<string,string> Rules, Dictionary<string, string> Optional =null)
+       {
+            Dictionary<string, object> ParaValidation = new Dictionary<string, object>();
+            foreach (KeyValuePair<string, string> entry in Rules)
+            {
+                var rule = entry.ToString().Split();
+                System.Reflection.PropertyInfo pi = Request.GetType().GetProperty(entry.Key);
+                String fieldValue = (String)(pi.GetValue(Request, null));
+                ParaValidation.Add( entry.Value, fieldValue);
+            }
+            
+
+
+        }
+    }
+}
--- ValidationFramework/WebApp/Controllers/ValidationController.cs
+++ ValidationFramework/WebApp/Controllers/ValidationController.cs
@@ -4,24 +4,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ValidationFramework;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class ValidationController : Controller
     {
+        private static Validation validation = new Validation();
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(JObject form)
+        public IActionResult Index(Form form)
         {
             string message = string.Empty;
             try
             {
-              
-                
-            }
+
+                var Rules = new Dictionary<string, string>()
+                {
+                    {"Email","Email"},
+                    {"Password", "Password"}
+
+                };
+                validation.Validator(form, Rules);
+
+
+            }               
+            
             catch (Exception ex)
             {
 
